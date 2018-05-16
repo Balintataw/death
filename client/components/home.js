e998e633413de9ef9cb0da955420846b584f5107
balintataw <balintataw2@gmail.com>
2018-05-16 09:40:41 -0700

    grabs random messages at interval

--- client/components/home.js
+++ client/components/home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View, StatusBar, Image } from 'react-native'
 import {Actions} from 'react-native-router-flux'
 
+// const API = 'http://192.168.50.34:3001/api/getquote'
+// const API = 'http://localhost:3001/getquote'
+const API = 'http://10.68.0.239:3001/api/getquote'
+
 const styles = StyleSheet.create({
   container: {
       backgroundColor: 'rgb(126, 209, 85)',
@@ -21,6 +25,8 @@ const styles = StyleSheet.create({
   }
 });
 
+
+
 class Home extends Component {
   static defaultProps = {
 
@@ -29,31 +35,33 @@ class Home extends Component {
     response: ''
   }
 
-  componentDidMount() {
-
-
-    const API = 'http://192.168.50.34:3001/api/hello'
-    // const API = 'http://localhost:3001/getquote'
-    // const API = 'http://10.68.0.239:3001/'
+  getQuote = () => {
     return fetch(API, {
-           method: 'GET',headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-              }
-        }).then(response => response.json())
-            .then(resp => {
-              console.log('resp',resp.quote.quotes)
-              this.setState({
-                response: resp.quote.quotes
-              })
-            }).catch((error) => {
-              console.error(error);
-            })
+      method: 'GET',headers: {
+           Accept: 'application/json',
+           'Content-Type': 'application/json',
+         }
+   }).then(response => response.json())
+       .then(resp => {
+         console.log('resp',resp.quote.quotes)
+         this.setState({
+           response: resp.quote.quotes
+         })
+       }).catch((error) => {
+         console.error(error);
+       })
+  }
+
+  componentDidMount = () => {
+    this.getQuote()
+    setInterval(() => {
+      this.getQuote()
+    },3000)
   }     
 
 
-
   render() {
+
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
--- server.js
+++ server.js
@@ -5,13 +5,24 @@ const conn = require('./lib/conn')
 
 const port = process.env.PORT || 3001;
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/getquote', (req, res) => {
 //   res.send({ express: 'Hello From Express' });
 
   const sql = `
     SELECT * FROM quotes
   `
-  conn.query(sql, (err, results, fields) => {
+  const sqlRandom = `
+   SELECT quotes
+   FROM quotes AS q1 JOIN
+        (SELECT CEIL(RAND() *
+                    (SELECT MAX(id)
+                        FROM quotes)) AS id)
+        AS q2
+    WHERE q1.id >= q2.id
+    ORDER BY q1.id ASC
+    LIMIT 1;
+  `
+  conn.query(sqlRandom, (err, results, fields) => {
       console.log('results',results)
       res.json({
           quote: results[0]
